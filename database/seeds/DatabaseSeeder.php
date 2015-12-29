f5cb2d2d3181f38bd1da859de31d3d3b9df37688
Mendizalea <vagrant@homestead>
2015-12-29 20:30:06 +0000

    Truncating tables before executing the seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,19 +2,40 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
 	public function run()
 	{
 		Model::unguard();
 
+		$this->truncateTables(array(
+			'users',
+			'password_resets',
+			'tickets',
+			'ticket_votes',
+			'ticket_comments'
+		));
+
 		$this->call('UserTableSeeder');
+	}
+
+	private function truncateTables(array $tables)
+	{
+		$this->checkForeignkeys(false);
+
+		foreach ($tables as $table) {
+			DB::table($table)->truncate();
+		}
+
+		$this->checkForeignkeys(true);
+	}
+
+	private function checkForeignkeys($check)
+	{
+		$check = $check ? '1' : '0';
+		DB::statement("SET FOREIGN_KEY_CHECKS = $check;");
 	}
 
 }
